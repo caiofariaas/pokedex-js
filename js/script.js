ae9b66309fef94f622e3a7f9c5541a32748a44b3
caiofariaas <caiofarias077@gmail.com>
2023-11-24 15:54:12 -0300

    js

--- js/script.js
+++ js/script.js
@@ -25,18 +25,35 @@ const fetchPokemon = async (pokemon) =>{
     }
 }
 
+// Função para renderizar o pokemon na tela
+
 const renderPokemon = async (pokemon) =>{
+
+// Declaramos antes para a função não ficar em vazio enquanto o pokemon não é carregado
 
     pokeName.innerHTML = "Loading...";
     pokeNumber.innerHTML = '';
 
+// usamos o await aqui para esperar que todas as informações da func fetchPoke
+
     const data = await fetchPokemon(pokemon);
 
+// Aqui é um if que verifica se existe algo na variável data, caso não exista ele entra no else e printa 'none'
+
     if(data){
+
+// Carrega os itens do pokemon na tela
+
         pokeImg.style.display = 'block';
         pokeName.innerHTML = data.name;
         pokeNumber.innerHTML = data.id;
+
+// Caminho dos Gifs no retorno da API
+
         pokeImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+// Para após pesquisar o input ser limpo
+
         input.value = ''
         searchPoke = data.id;
     }
@@ -47,6 +64,9 @@ const renderPokemon = async (pokemon) =>{
         input.value = ''
     }
 }
+
+// aqui colocamos um EventListener para o 'submit' do form
+// que chama a func renderPokemon()
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
